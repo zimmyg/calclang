cee94728d40e578bf1187e5f430328cf4501821f
Tim Gleeson <tim.gleeson@yellowfin.bi>
2018-03-28 13:50:51 +1100

    add artifact for calclang lib

--- src/com/zim/antlr/grammar/calcfield/parsedexpression/Expression.java
+++
@@ -1,7 +0,0 @@
-package com.zim.antlr.grammar.calcfield.parsedexpression;
-
-import com.zim.antlr.grammar.calcfield.context.FieldDataType;
-
-public abstract class Expression {
-    public abstract FieldDataType getResultType() throws Exception;
-}
